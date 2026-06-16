f5474473599d9fa86b15b65c373adb2bd998f4cd
Zhenting Zhang <zhenting.zhang@student.unsw.edu.au>
2026-06-16 21:34:30 +1000

    添加vercel analytics
    Signed-off-by: Zhenting Zhang <zhenting.zhang@student.unsw.edu.au>

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ConfigProvider } from 'antd';
 import App from './App';
 import 'antd/dist/reset.css';
 import './index.css';
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
